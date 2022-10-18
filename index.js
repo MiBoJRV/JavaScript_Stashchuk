949a5de4a500f6916cbfc7f43f4c99b46267ca48
Mykhailo Bodnar <bodnar.mikhail@gmail.com>
2022-10-18 22:05:34 +0300

    JavaScript Scope

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-function printMyName() {
-    console.log('Mika');
+let a;
+let b;
+let c;
+
+function myFn() {
+    let b;
+    let c = 7;
+    a = true;
+    b = 10;
+    // console.log(b);
+    // return b;
 }
-console.log('Start');
-setTimeout(printMyName, 1000)
+
+myFn();
+myFn();
+myFn();
+
+console.log(a);
+console.log(b)
+console.log(c)
+
+
+// const ab = 5;
+//
+// function myFn1() {
+//     function innerFn() {
+//         console.log(ab);
+//     }
+//     innerFn();
+// }
+//
+// myFn1();
